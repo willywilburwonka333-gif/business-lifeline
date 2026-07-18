0eeb89ef97002e2d7093023ac33af54eaf8b099e
willywilburwonka333-gif <willywilburwonka333@gmail.com>
2026-07-18 11:16:51 +1000

    Load print report polish styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./tools.css";
+import "./print.css";
 
 export const metadata: Metadata = {
   title: "Business Lifeline | Business MRI",
