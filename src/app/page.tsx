0243f3ca5374cedf807d84854ec73a209fe17d24
willywilburwonka333-gif <willywilburwonka333@gmail.com>
2026-07-18 18:44:11 +1000

    Stage 7: mount one Business Lifeline application flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import { BusinessLifeline } from "@/components/business-lifeline";
-import { SavedReportGuard } from "@/components/saved-report-guard";
-import { SavedScenarioPlanner } from "@/components/saved-scenario-planner";
+import { BusinessLifelineApp } from "@/components/business-lifeline-app";
 
 export default function Home() {
   return (
     <div id="main-content" tabIndex={-1}>
-      <SavedReportGuard />
-      <BusinessLifeline />
-      <SavedScenarioPlanner />
+      <BusinessLifelineApp />
     </div>
   );
 }
