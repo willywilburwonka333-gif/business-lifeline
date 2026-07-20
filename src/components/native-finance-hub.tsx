ecd348c1dd2df92aa0bf01a16d967ddf262f323b
willywilburwonka333-gif <willywilburwonka333@gmail.com>
2026-07-21 09:33:10 +1000

    Show automatic accounting status in finance

--- src/components/native-finance-hub.tsx
+++ src/components/native-finance-hub.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { AdvancedAccountingHub } from "@/components/advanced-accounting-hub";
+import { OperatingLedgerStatus } from "@/components/operating-ledger-status";
 
 export function NativeFinanceHub() {
-  return <AdvancedAccountingHub />;
+  return <div className="native-finance-connected"><OperatingLedgerStatus /><AdvancedAccountingHub /></div>;
 }
