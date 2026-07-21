98340e05d48ad47578ddca7965c390a26ca36566
willywilburwonka333-gif <willywilburwonka333@gmail.com>
2026-07-21 10:45:52 +1000

    Add document vault beneath commercial operating flow

--- src/components/business-operating-platform.tsx
+++ src/components/business-operating-platform.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { CommercialOperatingPlatform } from "@/components/commercial-operating-platform";
+import { DocumentVaultMigrationCentre } from "@/components/document-vault-migration-centre";
 
 export function BusinessOperatingPlatform() {
-  return <CommercialOperatingPlatform />;
+  return <div className="business-operating-stack"><CommercialOperatingPlatform /><DocumentVaultMigrationCentre /></div>;
 }
